05e60e85a6426c212b5564fe0dd433bde9a03c80
ahmad.albaqir <ahmad.albaqir@gojek.com>
2022-07-31 23:34:28 +0700

    Build

--- components/navbar.js
+++ components/navbar.js
@@ -16,24 +16,23 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
-const LinkItem = ({href, path, children}) => {	
-	const active = path == href 
-	const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
-	return (
-		<NextLink href={href}>
-			<Link
-				p={2}
-				bg={active ? 'grassTeal' : undefined}
-				color={active ? '#202023' : inactiveColor}
-			>
-				{children}
-			</Link>
-		</NextLink>
-	)
-}
+// const LinkItem = ({href, path, children}) => {	
+// 	const active = path == href 
+// 	const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+// 	return (
+// 		<NextLink href={href}>
+// 			<Link
+// 				p={2}
+// 				bg={active ? 'grassTeal' : undefined}
+// 				color={active ? '#202023' : inactiveColor}
+// 			>
+// 				{children}
+// 			</Link>
+// 		</NextLink>
+// 	)
+// }
 
 const Navbar = props => {
-	const { path } = props
 	return (
 		<Box
 			position="fixed"
